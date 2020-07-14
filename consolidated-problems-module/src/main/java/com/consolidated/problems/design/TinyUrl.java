666af8bef600e68182ff472519cef84dc28e9569
prabu <prabueee.b@gmail.com>
2020-07-14 16:06:40 -0400

    Sweepline and tiny url problem updates

--- consolidated-problems-module/src/main/java/com/consolidated/problems/design/TinyUrl.java
+++ consolidated-problems-module/src/main/java/com/consolidated/problems/design/TinyUrl.java
@@ -4,24 +4,23 @@ import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 public class TinyUrl {
 	public static void main(String[] args) {
 		System.out.println("Approach4: Tiny Url: ");
-		Codec4 ob4 = new Codec4();
+		Codec3 ob4 = new Codec3();
 		String longUrl = "https://leetcode.com/problems/design-tinyurl";
 
 		String tinyUrl = ob4.encode(longUrl);
 		System.out.println("Encoded Url: " + tinyUrl);
-		System.out.println("Decoded Url: " + ob4.encode(tinyUrl));
+		System.out.println("Decoded Url: " + ob4.decode(tinyUrl));
 	}
 }
 
 // Approach 1- Using simple counter
 class Codec1 {
-	Map<Integer, String>	map	= new HashMap<>();
-	int						i	= 0;
+	Map<Integer, String> map = new HashMap<>();
+	int i = 0;
 
 	// Encodes a URL to a shortened URL.
 	public String encode(String longUrl) {
@@ -50,25 +49,47 @@ class Codec2 {
 	}
 }
 
-// Approach 3- using random function
+// Approach 3- using Base62 conversion -> 6 digit short url
 class Codec3 {
-	Map<Integer, String>	map	= new HashMap<>();
-	Random					r	= new Random();
-	int						key	= r.nextInt(10000);
+	private Map<String, String> longUrlMap = new HashMap<>();
+	private Map<String, String> shortUrlMap = new HashMap<>();
+	private static final String HOST = "http://tinyurl.com/";
+	private static final String SEED = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 	public String encode(String longUrl) {
-		while (map.containsKey(key))
-			key = r.nextInt(10000);
-		map.put(key, longUrl);
-		return "http://tinyurl.com/" + key;
+		if (longUrlMap.containsKey(longUrl))
+			return HOST + longUrlMap.get(longUrl);
+
+		String encodedUrl = base62Conversion();
+		longUrlMap.put(longUrl, encodedUrl);
+		shortUrlMap.put(encodedUrl, longUrl);
+
+		return HOST + encodedUrl;
 	}
 
 	public String decode(String shortUrl) {
-		return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+		//String encodedUrl = shortUrl.replace("http://tinyurl.com/", "");
+		//return shortUrlMap.get(encodedUrl);
+		//or
+		return shortUrlMap.get(shortUrl.substring(HOST.length()));
 	}
 
+	private String base62Conversion() {
+		StringBuilder sb = null;
+		do {
+			sb = new StringBuilder();
+			for (int i = 0; i < 6; i++) {
+				int random = (int) (Math.random() * SEED.length());
+				System.out.println(random);
+				sb.append(SEED.charAt(random));
+			}
+		} while (shortUrlMap.containsKey(sb.toString()));
+
+		return sb.toString();
+	}
 }
 
+//Approach4: using Base64 conversion 
 class Codec4 {
 	public String encode(String longUrl) {
 		return Base64.getUrlEncoder().encodeToString(longUrl.getBytes(StandardCharsets.UTF_8));
--- problem-patterns-module/src/main/java/com/problems/patterns/SweepLinePatterns.java
+++ problem-patterns-module/src/main/java/com/problems/patterns/SweepLinePatterns.java
@@ -1,93 +1,112 @@
 package com.problems.patterns;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.TreeMap;
 
+/*
+ * SweepLine:
+ * A sweep line is an imaginary vertical line which is swept across the plane rightwards. That's why, the algorithms based on this concept are 
+ * sometimes also called plane sweep algorithms. We sweep the line based on some events, in order to discretize the sweep.
+ * In computational geometry, a sweep line algorithm or plane sweep algorithm is an algorithmic paradigm that uses a conceptual sweep line or 
+ * sweep surface to solve various problems in Euclidean space. It is one of the key techniques in computational geometry. The idea behind algorithms 
+ * of this type is to imagine that a line (often a vertical line) is swept or moved across the plane, stopping at some points.
+ */
 public class SweepLinePatterns {
 
-	//	The Skyline Problem
-	/* Skyline problem without any additional object 
+	//TODO: Closest Pair of Points:
+	//Ref: https://www.geeksforgeeks.org/closest-pair-of-points-onlogn-implementation/
+
+	//TODO: Line Segment Intersection Problem:
+	//Ref: https://www.geeksforgeeks.org/given-a-set-of-line-segments-find-if-any-two-segments-intersect/
+
+	//TODO: Remove Covered Intervals - Leet code 
+	public int removeCoveredIntervals(int[][] A) {
+		int res = 0, right = 0;
+		Arrays.sort(A, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
+		for (int[] v : A) {
+			if (v[1] > right) {
+				++res;
+				right = v[1];
+			}
+		}
+		return res;
+	}
+
+	// The Skyline Problem
+	/* Approaches:
+	 * Skyline problem without any additional object 
 	 * SkyLine Problem using Priority Queue: TimeComplexity:  O(n^2) (Here n - no of building points)
 	 * SkyLine Problem using TreeMap: TimeComplexity: O(nlogn);
 	 */
+	// Solution using Priority Queue - O(n^2) operation
 	public List<int[]> getSkyline1(int[][] buildings) {
 		List<int[]> points = new ArrayList<>();
 		List<int[]> result = new ArrayList<>();
-		for (int i = 0; i < buildings.length; i++) {
-			points.add(new int[] { buildings[i][0],
-					buildings[i][2] });
-			points.add(new int[] { buildings[i][1],
-					-buildings[i][2] });
+		for (int[] building : buildings) {
+			points.add(new int[] { building[0], building[2] });
+			points.add(new int[] { building[1], -building[2] });
 		}
 		// Using lambda expression
-		Collections.sort(points, (a, b) -> {
-			if (a[0] != b[0]) return a[0] - b[0];
-			return b[1] - a[1];
-		});
+		Collections.sort(points, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
 
-		// Solution using Priority Queue - O(n^2) operation
-		PriorityQueue<Integer> queue = new PriorityQueue<>(
-				Collections.reverseOrder());
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
 		queue.add(0);
-		int prev = 0, curr = 0;
+		int prevHeight = 0, currHeight = 0;
 		for (int[] point : points) {
 			if (point[1] < 0) {
 				queue.remove(-point[1]);
 			} else {
 				queue.add(point[1]);
 			}
-			curr = queue.peek();
-			if (prev != curr) {
-				result.add(new int[] { point[0], curr });
-				prev = curr;
+			currHeight = queue.peek();
+			if (prevHeight != currHeight) {
+				result.add(new int[] { point[0], currHeight });
+				prevHeight = currHeight;
 			}
 		}
 		return result;
 	}
 
+	// Solution using Tree Map - O(nlogn) operation
 	public List<int[]> getSkyline2(int[][] buildings) {
 		List<int[]> points = new ArrayList<>();
 		List<int[]> result = new ArrayList<>();
-		for (int i = 0; i < buildings.length; i++) {
-			points.add(new int[] { buildings[i][0],
-					buildings[i][2] });
-			points.add(new int[] { buildings[i][1],
-					-buildings[i][2] });
+		for (int[] building : buildings) {
+			points.add(new int[] { building[0], building[2] });
+			points.add(new int[] { building[1], -building[2] });
 		}
 		// Using lambda expression
-		Collections.sort(points, (a, b) -> {
-			if (a[0] != b[0]) return a[0] - b[0];
-			return b[1] - a[1];
-		});
-		// Solution using Tree Map - O(nlogn) operation
+		Collections.sort(points, (a, b) -> a[0] != b[0] ? a[0] - b[0] : b[1] - a[1]);
+
 		TreeMap<Integer, Integer> map = new TreeMap<>();
 		map.put(0, 1);
-		int prev = 0, curr = 0;
+		int prevHeight = 0, currHeight = 0;
 		for (int[] point : points) {
 			if (point[1] < 0) {
 				int value = map.get(-point[1]);
 				if (value > 1)
 					map.put(-point[1], value - 1);
-				else map.remove(-point[1]);
+				else
+					map.remove(-point[1]);
 			} else {
-				map.put(point[1],
-						map.getOrDefault(point[1], 0) + 1);
+				map.put(point[1], map.getOrDefault(point[1], 0) + 1);
 			}
-			curr = map.lastKey();
-			if (prev != curr) {
-				result.add(new int[] { point[0], curr });
-				prev = curr;
+			currHeight = map.lastKey();
+			if (prevHeight != currHeight) {
+				result.add(new int[] { point[0], currHeight });
+				prevHeight = currHeight;
 			}
 		}
 		return result;
 	}
 
 	//	Rectangle Area II
+	//	Perfect Rectangle
 	//	Time Intersection
 	//	Number of Airplanes in the Sky
-	//	Perfect Rectangle
 
 }
